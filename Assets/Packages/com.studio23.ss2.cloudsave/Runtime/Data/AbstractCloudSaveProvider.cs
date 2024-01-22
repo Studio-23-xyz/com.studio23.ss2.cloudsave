a7fa563b587cad4ed68f5ba46932001af4b1176f
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2024-01-22 19:33:39 +0600

    ## [v2.0.0] - 2024-1-22
    #  Changelog
    
    - Cloud Save provider is now based on Factory Pattern
    - Editor Script added to make developers life easy
    - Platform Selection happen automatically based on Popular Scripting Symboles
    - Added a dummyProvider for Debug Purposes

--- Assets/Packages/com.studio23.ss2.cloudsave/Runtime/Data/AbstractCloudSaveProvider.cs
+++ Assets/Packages/com.studio23.ss2.cloudsave/Runtime/Data/AbstractCloudSaveProvider.cs
@@ -2,30 +2,43 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 
-namespace Studio23.SS2.CloudSave.Core
+namespace Studio23.SS2.CloudSave.Data
 {
-    [RequireComponent(typeof(CloudSaveManager))]
-    public abstract class AbstractCloudSaveProvider : MonoBehaviour
+    public abstract class AbstractCloudSaveProvider : ScriptableObject
     {
+        [SerializeField]
+        protected PlatformProvider _platformProvider;
 
+        public PlatformProvider PlatformProvider
+        {
+            get
+            {
+                return _platformProvider;
+            }
+
+            set
+            {
+                _platformProvider = value;
+            }
+
+        }
         /// <summary>
         /// This is responsible for initialization of platform cloud save feature if needed
         /// </summary>
         protected internal abstract UniTask<int> Initialize();
-
         /// <summary>
         /// You should fire OnUploadSuccess in the implementation.
         /// </summary>
         /// <param name="key"></param>
         /// <param name="filepath"></param>
-        protected internal abstract UniTask<int> UploadToCloud(string key, byte[] data);
+        protected internal abstract UniTask<int> UploadToCloud(string slotName,string key, byte[] data);
 
         /// <summary>
         /// You should fire OnDownloadSuccess in the implementation
         /// </summary>
         /// <param name="key"></param>
         /// <param name="downloadLocation"></param>
-        protected internal abstract UniTask<byte[]> DownloadFromCloud(string key);
+        protected internal abstract UniTask<byte[]> DownloadFromCloud(string slotName,string key);
 
 
     }
